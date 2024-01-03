5a981bd07821a53e82a26b7f25605a849af07e13
apalazzomusic <apalazzomusic@gmail.com>
2024-01-02 19:36:45 -0800

    builds initial jest testing suite for statusReducer

---
+++ client/__tests__/clientTest.js
@@ -0,0 +1,18 @@
+import subject from '../src/utils/reducers/statusSlice';
+
+describe('statusReducer', () => {
+    let state;
+
+    beforeEach(() => {
+        state = {
+            page: 'LANDING_PAGE',
+            user: null,
+        };
+    });
+
+    describe('default state', () => {
+        it('should return a default state when given an undefined input', () => {
+          expect(subject(undefined, { type: undefined })).toEqual(state);
+        });
+      });
+});
