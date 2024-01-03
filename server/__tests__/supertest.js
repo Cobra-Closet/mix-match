dea32cddbb5951f6795553361a1bec1d22cf2971
apalazzomusic <apalazzomusic@gmail.com>
2024-01-03 11:09:34 -0800

    tests server route handling known/unknown requests

--- server/__tests__/supertest.js
+++ server/__tests__/supertest.js
@@ -13,4 +13,49 @@ describe('Route integration', () => {
             });
         });
     });
+
+    describe('/user', () => {
+        describe('GET', () => {
+            it('responds with 200 status and text/html content type', () => {
+                return request(server)
+                    .get('/user')
+                    .expect('Content-Type', /text\/html/)
+                    .expect(200);
+            })
+        })
+    })
+
+    describe('/wobbedrobe', () => {
+        describe('GET', () => {
+            it('responds with 200 status and text/html content type', () => {
+                return request(server)
+                    .get('/wobbedrobe')
+                    .expect('Content-Type', /text\/html/)
+                    .expect(200);
+            })
+        })
+    })
+
+    describe('/ootd', () => {
+        describe('GET', () => {
+            it('responds with 200 status and text/html content type', () => {
+                return request(server)
+                    .get('/ootd')
+                    .expect('Content-Type', /text\/html/)
+                    .expect(200);
+            })
+        })
+    })
+
+    describe('Unknown route handler', () => {
+        describe('POST', () => {
+            it('responds with 404 status and "Not Found" message', () => {
+                return request(server)
+                    .post('/some-invalid-route')
+                    .expect('Content-Type', /text\/html/)
+                    .expect(404);
+
+            })
+        })
+    })
 })
