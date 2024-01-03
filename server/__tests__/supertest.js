9db4b560c4cb2eefec63bed90c4905cf27ba1bf8
apalazzomusic <apalazzomusic@gmail.com>
2024-01-03 11:28:59 -0800

    cleans route test, removes depricated code

--- server/__tests__/supertest.js
+++ server/__tests__/supertest.js
@@ -49,13 +49,39 @@ describe('Route integration', () => {
 
     describe('Unknown route handler', () => {
         describe('POST', () => {
-            it('responds with 404 status and "Not Found" message', () => {
+            it('responds with 404 status', async () => {
                 return request(server)
                     .post('/some-invalid-route')
                     .expect('Content-Type', /text\/html/)
                     .expect(404);
+            })
+        })
+        describe('DELETE', () => {
+            it('responds with 404 status', async () => {
+                return request(server)
+                    .delete('/some-invalid-route')
+                    .expect('Content-Type', /text\/html/)
+                    .expect(404);
+            })
+        })
+        describe('PUT', () => {
+            it('responds with 404 status', async () => {
+                return request(server)
+                    .put('/some-invalid-route')
+                    .expect('Content-Type', /text\/html/)
+                    .expect(404);
+            })
+        })
 
+        describe('GET', () => {
+            it('responds with 200 status and text/html content type', () => {
+                return request(server)
+                    .get('/some-invalid-route')
+                    .expect('Content-Type', /text\/html/)
+                    .expect(200);
             })
         })
     })
+
+    
 })
